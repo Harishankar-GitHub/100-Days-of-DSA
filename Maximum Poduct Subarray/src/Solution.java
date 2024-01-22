095bb728fec6c7476d095c76ee974a6a07921c30
Harishankar Bhat R <rhsb1997@gmail.com>
2024-01-22 19:33:35 +0530

    Added 1 more solution

--- Maximum Poduct Subarray/src/Solution.java
+++ Maximum Poduct Subarray/src/Solution.java
@@ -3,14 +3,20 @@ public class Solution {
     // Problem statement: https://leetcode.com/problems/maximum-product-subarray/
 
     public static void main(String[] args) {
-        System.out.println(maxProduct(new int[]{2, 3, -2, 4}));
-        System.out.println(maxProduct(new int[]{-2, 0, -1}));
-        System.out.println(maxProduct(new int[]{0, 2}));
+        System.out.println("Solution 1");
+        System.out.println(maxProduct_solution_1(new int[]{2, 3, -2, 4}));
+        System.out.println(maxProduct_solution_1(new int[]{-2, 0, -1}));
+        System.out.println(maxProduct_solution_1(new int[]{0, 2}));
+
+        System.out.println("Solution 2");
+        System.out.println(maxProduct_solution_2(new int[]{2, 3, -2, 4}));
+        System.out.println(maxProduct_solution_2(new int[]{-2, 0, -1}));
+        System.out.println(maxProduct_solution_2(new int[]{0, 2}));
     }
 
-    public static int maxProduct(int[] nums) {
+    public static int maxProduct_solution_1(int[] nums) {
 
-        // O(n) time | O(n) space
+        // O(n) time | O(1) space
 
         if (nums.length == 1) return nums[0];
 
@@ -39,5 +45,27 @@ public class Solution {
         }
 
         return maxSoFar;
+    }
+
+    public static int maxProduct_solution_2(int[] nums) {
+
+        // O(n) time | O(1) space
+
+        int length = nums.length;
+        int leftProduct = 1;
+        int rightProduct = 1;
+        int maxProductSoFar = nums[0];
+
+        for (int i = 0; i < length; i++) {
+            leftProduct = leftProduct * nums[i];
+            rightProduct = rightProduct * nums[length - i - 1];
+
+            maxProductSoFar = Math.max(maxProductSoFar, Math.max(leftProduct, rightProduct));
+
+            leftProduct = leftProduct == 0 ? 1 : leftProduct;
+            rightProduct = rightProduct == 0 ? 1 : rightProduct;
+        }
+
+        return maxProductSoFar;
     }
 }
