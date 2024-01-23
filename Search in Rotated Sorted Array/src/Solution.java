b51a48edb526c0990a1c3bfa4047bbfa1939ac1e
Harishankar Bhat R <rhsb1997@gmail.com>
2024-01-23 11:21:40 +0530

    Added 1 more solution

--- Search in Rotated Sorted Array/src/Solution.java
+++ Search in Rotated Sorted Array/src/Solution.java
@@ -3,12 +3,18 @@ public class Solution {
     // Problem statement: https://leetcode.com/problems/search-in-rotated-sorted-array/
 
     public static void main(String[] args) {
-        System.out.println(search(new int[]{4,5,6,7,0,1,2}, 0));    // 4
-        System.out.println(search(new int[]{4,5,6,7,0,1,2}, 3));    // -1
-        System.out.println(search(new int[]{1}, 0));    // -1
+        System.out.println("Search Solution 1");
+        System.out.println(search_solution_1(new int[]{4,5,6,7,0,1,2}, 0));    // 4
+        System.out.println(search_solution_1(new int[]{4,5,6,7,0,1,2}, 3));    // -1
+        System.out.println(search_solution_1(new int[]{1}, 0));    // -1
+
+        System.out.println("Search Solution 2");
+        System.out.println(search_solution_2(new int[]{4,5,6,7,0,1,2}, 0));    // 4
+        System.out.println(search_solution_2(new int[]{4,5,6,7,0,1,2}, 3));    // -1
+        System.out.println(search_solution_2(new int[]{1}, 0));    // -1
     }
 
-    public static int search(int[] nums, int target) {
+    public static int search_solution_1(int[] nums, int target) {
         int ans = -1;
         int start = 0;
         int end = nums.length - 1;
@@ -36,5 +42,49 @@ public class Solution {
         }
 
         return ans;
+    }
+
+    public static int search_solution_2(int[] nums, int target) {
+        if (nums.length == 0) return -1;
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        // This while loop is to find out the index of minimum number in the array.
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        int indexOfMinimumNumber = left;
+        left = 0;
+        right = nums.length - 1;
+
+        // This condition is to find out whether to search in left half of the array or to the right.
+        if (target >= nums[indexOfMinimumNumber] && target <= nums[right]) {
+            left = indexOfMinimumNumber;
+        } else {
+            right = indexOfMinimumNumber;
+        }
+
+        // After finding out which half of the array to search, the below code is usual binary search to search through the array.
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
     }
 }
